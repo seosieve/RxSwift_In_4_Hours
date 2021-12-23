b78add9bef93c15350571e7e02c06f22fb5fec3e
seosieve <royalcircle97@naver.com>
2021-12-24 01:07:33 +0900

    SugarAPI Finish

--- Example/season2/step1/RxSwift+MVVM/ViewController.swift
+++ Example/season2/step1/RxSwift+MVVM/ViewController.swift
@@ -15,12 +15,19 @@ let MEMBER_LIST_URL = "https://my.api.mockaroo.com/members_with_avatar.json?key=
 class ViewController: UIViewController {
     @IBOutlet var timerLabel: UILabel!
     @IBOutlet var editView: UITextView!
+    
+    var disposable: Disposable?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
             self?.timerLabel.text = "\(Date().timeIntervalSince1970)"
         }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        disposable?.dispose()
     }
 
     private func setVisibleWithAnimation(_ v: UIView?, _ s: Bool) {
@@ -31,6 +38,40 @@ class ViewController: UIViewController {
             self?.view.layoutIfNeeded()
         })
     }
+    
+//    func downloadJson(_ url: String) -> Observable<String?> {
+//        return Observable.create { emitter in
+//            emitter.onNext("Hello World")
+//            emitter.onCompleted()
+//            return Disposables.create()
+//        }
+//    }
+    
+    //Sugar API
+//    func downloadJson(_ url: String) -> Observable<String?> {
+//        Observable.from(["Hello","World"])
+//    }
+
+    func downloadJson(_ url: String) -> Observable<String?> {
+        return Observable.create { emitter in
+            let url = URL(string: url)!
+            let task = URLSession.shared.dataTask(with: url) { data, _, err in
+                guard err == nil else {
+                    emitter.onError(err!)
+                    return
+                }
+                if let dat = data, let json = String(data: dat, encoding: .utf8) {
+                    emitter.onNext(json)
+                }
+                emitter.onCompleted()
+            }
+            task.resume()
+            return Disposables.create() {
+                task.cancel()
+            }
+            
+        }
+    }
 
     // MARK: SYNC
 
@@ -39,12 +80,13 @@ class ViewController: UIViewController {
     @IBAction func onLoad() {
         editView.text = ""
         setVisibleWithAnimation(activityIndicator, true)
-
-        let url = URL(string: MEMBER_LIST_URL)!
-        let data = try! Data(contentsOf: url)
-        let json = String(data: data, encoding: .utf8)
-        self.editView.text = json
-        
-        self.setVisibleWithAnimation(self.activityIndicator, false)
+        let jsonObservable = downloadJson(MEMBER_LIST_URL)
+        let helloObservable = Observable.just("Hello World")
+        disposable = Observable.zip(jsonObservable, helloObservable) {$1 + "\n" + $0!}
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { json in
+                    self.editView.text = json
+                    self.setVisibleWithAnimation(self.activityIndicator, false)
+            })
     }
 }
